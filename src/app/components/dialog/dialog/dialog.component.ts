5c0aa74b59a1ec4856c5af0776cd4b7ef32b276d
Brayan Correa <brayan_seyer@hotmail.com>
2022-10-07 09:01:27 -0500

    ya guarda en el localstorage el elemento completo

--- src/app/components/dialog/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog/dialog.component.ts
@@ -10,28 +10,26 @@ export class DialogComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<DialogComponent>) { }
 
-  favoriteList: any[] = [];
+  //favoriteList: any[] = [];
 
   ngOnInit(): void {
-    console.log("data recibida: ", this.data);
+
   }
 
   addToList(data:any){
-    console.log("ya sirve el boton agregar a la fila");
-    localStorage.setItem("movie-list",JSON.stringify(data));
-    let getfromLocal = localStorage.getItem("movie-list");
-    console.log("traido del localstorage", getfromLocal);
-    /*this.favoriteList.push(getfromLocal);
-    console.log("arreglo con peliculas: ", this.favoriteList);
-    let indexOfList = this.favoriteList.indexOf(getfromLocal);
-    console.log(indexOfList);
-    let indexList = this.favoriteList.length;
-    console.log("indexList",indexList);
-    this.favoriteList.splice(indexOfList,0)
-    console.log(this.favoriteList);*/
-    
-  }
+    let favoriteList= JSON.parse(localStorage.getItem('movie-list') ?? '[]');
 
+    console.log("ya sirve el boton agregar a la fila, esta info me pasaron: ", data);
+    if(favoriteList.indexOf(data) == -1){
+      favoriteList.push(data);
+      console.log(JSON.stringify(favoriteList));
+    localStorage.setItem("movie-list",JSON.stringify(favoriteList));
+    }
+  
+  
+  }
+  
+  
   markAsFavorite(data:any){
     console.log("ya sirve el boton favorito");
     localStorage.setItem("movie-fav",JSON.stringify(data));
